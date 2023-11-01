0a34009479a014f217a9fe1c4f7957dddd2bb57b
Carlos Eduardo <carlao0010@hotmail.com>
2023-10-31 21:15:32 -0300

    Req 04 OK

---
+++ src/scripts/functionModal.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const openButton = document.getElementById('btn-modal');
+const closeButton = document.getElementById('close');
+
+openButton.addEventListener('click', () => {
+  modal.style.display = 'block';
+  modalImage.src = './img/img-1.png'; // Atualize com o caminho da imagem
+});
+
+closeButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
